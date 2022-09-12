073ec619483a78229c9bf826459b0e994c434919
now <mahmoudeid127@gmail.com>
2022-09-12 09:25:03 -0700

    add chat and natification

--- lib/Screens/home/home_view_model.dart
+++ lib/Screens/home/home_view_model.dart
@@ -1,9 +1,17 @@
 
 
 import '../../base.dart';
+import '../../datebase/database_utils.dart';
+import '../../models/room.dart';
 import 'navigator.dart';
 
 class HomeViewModel extends BaseViewModel<HomeNavigator>{
 
+  List<Room> rooms=[];
+
+  void getRooms()async{
+    rooms= await DateBaseUtils.getRoomsFromFireStore();
+    print(rooms);
+  }
 
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
 
 import 'package:firebase_core/firebase_core.dart';
@@ -9,19 +11,117 @@ import 'package:untitled1/Screens/Login_Screen/Login.dart';
 import 'package:untitled1/providers/user_provider.dart';
 
 import 'Screens/add_rooms/add_room.dart';
+import 'Screens/chat/chat_screen.dart';
 import 'Screens/home/home.dart';
+FirebaseMessaging messaging = FirebaseMessaging.instance;
+FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+
+  await setupFlutterNotifications();
+  showFlutterNotification(message);
+  print("Handling a background message: ${message.messageId}");
+}
+late AndroidNotificationChannel channel;
+bool isFlutterLocalNotificationsInitialized = false;
+
+Future<void> setupFlutterNotifications() async {
+  if (isFlutterLocalNotificationsInitialized) {
+    return;
+  }
+  channel = const AndroidNotificationChannel(
+    'high_importance_channel', // id
+    'High Notification Chat', // title
+    description:
+    'This channel is used for important notifications.', // description
+    importance: Importance.high,
+  );
+
+  await flutterLocalNotificationsPlugin
+      .resolvePlatformSpecificImplementation<
+      AndroidFlutterLocalNotificationsPlugin>()
+      ?.createNotificationChannel(channel);
+
+  /// Update the iOS foreground notification presentation options to allow
+  /// heads up notifications.
+  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+    alert: true,
+    badge: true,
+    sound: true,
+  );
+  isFlutterLocalNotificationsInitialized = true;
+}
+void showFlutterNotification(RemoteMessage message) {
+  RemoteNotification? notification = message.notification;
+  AndroidNotification? android = message.notification?.android;
+  if (notification != null && android != null ) {
+    flutterLocalNotificationsPlugin.show(
+      notification.hashCode,
+      notification.title,
+      notification.body,
+      NotificationDetails(
+        android: AndroidNotificationDetails(
+          channel.id,
+          channel.name,
+          channelDescription: channel.description,
+          // TODO add a proper drawable resource to android, for now using
+          //      one that already exists in example app.
+          icon: 'launch_background',
+        ),
+      ),
+    );
+  }
+}
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+
+  print('User granted permission: ${settings.authorizationStatus}');
+
+  printTOkenData();
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  setupFlutterNotifications();
   runApp( MultiProvider(providers: [
     ChangeNotifierProvider<UserProvider>(create: (c) => UserProvider())
   ], child: MyApp()));
+
+}
+Future<String?> printTOkenData()async{
+  String? token = await messaging.getToken();
+  print("token:${token??""}");
+  return token;
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    FirebaseMessaging.onMessage.listen(showFlutterNotification);
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+
+    });
+  }
   @override
   Widget build(BuildContext context) {
     var provider = Provider.of<UserProvider>(context);
@@ -35,6 +135,7 @@ class MyApp extends StatelessWidget {
         LoginScreen.routName:(v)=>LoginScreen(),
         HomeScreen.routeName: (c) => HomeScreen(),
         AddRoom.routeName: (c) => AddRoom(),
+        ChatScreen.routeName:(c)=>ChatScreen(),
       },
     );
   }
